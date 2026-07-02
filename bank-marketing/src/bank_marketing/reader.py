e6b45bd33f0231006b3d5ff987260b86df6320d5
Avinash Tamarapalli <avinash12082001@gmail.com>
2026-07-02 22:57:46 +0530

    loading dataframes

--- bank-marketing/src/bank_marketing/reader.py
+++ bank-marketing/src/bank_marketing/reader.py
@@ -1,7 +1,20 @@
 import pandas as pd
-from utilies import PROJECT_ROOT_PATH
+from utilies import *
 
 DATA_ROOT_PATH = PROJECT_ROOT_PATH / "src" / "data"
 
 dataframes = {}
 
+data_entities = yaml_loader(PROJECT_ROOT_PATH / "src" / "dataEntities.yaml").get('dataEntities', [])
+
+
+
+for entity in data_entities:
+    entity_details = get_entity_details(entity)
+
+    if entity_details.get('format') == 'csv':
+        dataframes[entity] = csv_loader(entity_details.get('location'))
+    else:
+        dataframes[entity] = 'entity'
+
+print(dataframes.keys())
--- bank-marketing/src/bank_marketing/utilies.py
+++ bank-marketing/src/bank_marketing/utilies.py
@@ -16,3 +16,12 @@ def yaml_loader(file_path):
     with open(file_path, 'r') as file:
         data = yaml.safe_load(file)
     return data
+
+def get_entity_details(entity):
+    data_entities_details = yaml_loader(PROJECT_ROOT_PATH / "src" / "dataEntities.yaml").get('dataEntitiesDetails', {})
+    entity_details = data_entities_details.get(entity)
+
+    format = entity_details.get('format', '')
+    location = PROJECT_ROOT_PATH / entity_details.get('loc','')
+
+    return {'format': format, 'location': location}
